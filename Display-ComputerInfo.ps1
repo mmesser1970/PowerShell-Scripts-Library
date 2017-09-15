c756029cbdc04b56bb9dc40076c08e9bf9d97f32
mmesser1970 <mmesserlmt@gmail.com>
2017-09-14 21:58:55 -0400

    Update Display-ComputerInfo.ps1

--- Display-ComputerInfo.ps1
+++ Display-ComputerInfo.ps1
@@ -4,23 +4,23 @@
 
 .DESCRIPTION
 	Extracts local computer information and outputs the information into an easy-to-read window.
-
-.INSTRUCTIONS
-	-Create a batch file to run the PowerShell file using the following script:
-		@ECHO OFF
-		PowerShell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& '%~Dpn0.ps1'"
-	-Save the batch file in the same location as this PowerShell file using the EXACT same name as this file.
-	-Create a shortcut to the batch file in a central location (i.e. network drive).
-	-Copy the shortcut to end-user desktops.
 #>
 
-#@========================================================================================
-#@ Script Name: Display-ComputerInfo
-#@ Created: 02/21/2017
-#@ Updated: 09/12/2017
-#@ Originator: Matthew Messer
-#@ Email: mmesserlmt@gmail.com
-#@========================================================================================
+#========================================================================================
+# INSTRUCTIONS
+# -Create a batch file to run the PowerShell file using the following script:
+#	@ECHO OFF
+#	PowerShell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& '%~Dpn0.ps1'"
+# -Save the batch file in the same location as this PowerShell file using the EXACT same name as this file.
+# -Create a shortcut to the batch file in a central location (i.e. network drive).
+# -Copy the shortcut to end-user desktops.
+#========================================================================================
+# Script Name: Display-ComputerInfo
+# Created: 02/21/2017
+# Updated: 09/12/2017
+# Originator: Matthew Messer
+# Email: mmesserlmt@gmail.com
+#========================================================================================
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 
