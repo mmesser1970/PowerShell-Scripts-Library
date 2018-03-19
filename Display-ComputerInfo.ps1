862b4e8f2b4130678a45b1553688b004eea4ff1a
Matthew Messer <mmesserlmt@gmail.com>
2018-03-19 17:37:57 +0000

    Update Display-ComputerInfo.ps1

--- Display-ComputerInfo.ps1
+++ Display-ComputerInfo.ps1
@@ -22,25 +22,25 @@
 # Email: mmesserlmt@gmail.com
 #========================================================================================
 
+<#.Synopsis Display local computer information.DESCRIPTION Extracts local computer data and outputs the information into an easy-to-read window.#>
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+$UserDom = $env:userdomain
+$UserName  $env:username
+$User = (([ADSI]"WinNT://$UserDom/$UserName").FullName.ToString())
+$ComputerName = $env:computername
+$OSRegister = (Get-WmiObject Win32_OperatingSystem).RegisteredUser
+$IPAddress = ((IPConfig | FindStr [0-9].\.)[0]).Split()[-1]
+$MACAddress = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $ComputerName | where { $_.IPAddress -eq $IPAddress}).MACAddress
+$Manufacturer = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+$Model = (Get-WmiObject Win32_ComputerSystemProduct).Model
+$Version = (Get-WmiObject Win32_ComputerSystemProduct).Version
+$Name = (Get-WmiObject Win32_ComputerSystemProduct).Name
+$SerialNumber = (Get-WmiObject Win32_Bios).SerialNumber
 
-$UserDom            = $env:userdomain
-$UserName           = $env:username
-$User               = (([ADSI]"WinNT://$UserDom/$UserName").FullName.ToString())
-$ComputerName		= $env:computername
-$OSRegister         = (Get-WmiObject Win32_OperatingSystem).RegisteredUser
-$IPAddress			= ((IPConfig | FindStr [0-9].\.)[0]).Split()[-1]
-$Manufacturer		= (Get-WmiObject Win32_ComputerSystem).Manufacturer
-$Model				= (Get-WmiObject Win32_ComputerSystemProduct).Model
-$Version			= (Get-WmiObject Win32_ComputerSystemProduct).Version
-$SerialNumber		= (Get-WmiObject Win32_Bios).SerialNumber
-
-If ($Model){$ComputerModel = $Model}
-Else {$Computermodel = $Version}
-
-[System.Windows.Forms.MessageBox]::Show((" User: `t`t $($User)`r`n","Computer Name:`t $($ComputerName)`r`n","Registered To:`t $($OSRegister)`r`n",`
-"IP Address:`t $($IPAddress)`r`n",`
+If ($Model){$ComputerModel = $Model}Else {$Computermodel = $Name}
+[System.Windows.Forms.MessageBox]::Show((" User: `t`t $($User)`r`n","Computer Name:`t $($ComputerName)`r`n",`
+"Registered To:`t $($OSRegister)`r`n",`
+"IP Address:`t $($IPAddress)`r`n", "MAC Address:`t $($MACAddress)`r`n",`
 "Manufacturer: `t $($Manufacturer)`r`n","Model: `t`t $($ComputerModel)`r`n",`
 "Serial Number: `t $($SerialNumber)`r`n") , "Computer Information" ,`
-[System.Windows.Forms.MessageBoxButtons]::OK,`
-[System.Windows.Forms.MessageBoxIcon]::Information)
+[System.Windows.Forms.MessageBoxButtons]::OK,`[System.Windows.Forms.MessageBoxIcon]::Information)
