61cd8dbd6a94d01d16e5d75a061c2c48b045ea9a
Matthew Messer <mmesserlmt@gmail.com>
2018-08-16 09:48:53 -0400

    ADUser-ListToCSV.ps1

--- ADUser-ListToCSV.ps1
+++ ADUser-ListToCSV.ps1
@@ -1,16 +1,12 @@
-#==============================================================================================================================
-# Save this script in the "\\path-to-network-file\" location
-#==============================================================================================================================
-
 Import-Module ActiveDirectory
 
 $SCRIPT_PARENT = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-$Users = ForEach ($User in $(Get-Content "\\path-to-network-file\AllADUsers.txt"))
+$Users = ForEach ($User in $(Get-Content "C:\PathToFile\AllADUsers.txt"))
 
 {
 	Get-ADUser -Filter {displayName -eq $User} -Properties Department,Mail,telephoneNumber,Displayname
 }
 
 $Users | Select-Object SamAccountName, Displayname, telephoneNumber, Mail |`
-Export-CSV -Path "\\path-to-network-file\AllADUsers.csv" -NoTypeInformation
+
+Export-CSV -Path "C:\PathToFile\\AllADUsers.csv" -NoTypeInformation
