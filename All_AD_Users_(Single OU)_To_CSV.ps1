29e54f47f8d31b154a2d7d10f088666520b924df
Matthew Messer <mmesserlmt@gmail.com>
2018-08-21 09:10:08 -0400

    All_AD_Users_(Single OU)_To_CSV.ps1

--- All_AD_Users_(Single OU)_To_CSV.ps1
+++ All_AD_Users_(Single OU)_To_CSV.ps1
@@ -1,5 +1,5 @@
 # Change the OU "*Administrators*" to suit your needs (Keep the asterisk wild card)
 
-Get-ADUser -Filter {(Enabled -eq $true)} -SearchBase "OU=Computers,dc=Contoso,dc=com" |`
+Get-ADUser -Filter {(Enabled -eq $true)} -SearchBase "OU=Computers,dc=Contoso,dc=com" | `
 ? { ($_.distinguishedname -like "*Administrators*") } |`
 Export-Csv -Path C:\Temp\All-Admin-Users.csv
